7681f9ef6e5ae3c6860704fb58607de4c6f93505
Muhammad Tamjeed Hussain <muhammadtamjeedh@gmail.com>
2024-12-17 22:08:01 +0500

    first commit

---
+++ components/Helper/SectionHeading.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react"
+
+type Props = {
+    children: ReactNode;
+}
+const SectionHeading = ({children}:Props) => {
+    return (
+      <h1 className="bg-[#2c5282] w-fit text-center px-4 py-3 mx-auto text-white text-2xl sm:text-3xl md:text-4xl uppercase -rotate-6">
+        {children}
+      </h1>
+    );
+}
+
+export default SectionHeading
